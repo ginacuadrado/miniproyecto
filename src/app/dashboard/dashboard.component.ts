62f2f21fcacbfc81582aa939212bee7eafdf08b8
Gina Cuadrado <gcuadrado@correo.unimet.edu.ve>
2018-10-12 13:01:45 -0400

    Servicio incluido

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { LecturaService } from './lectura.service';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  providers: [LecturaService]
+
 })
+
 export class DashboardComponent implements OnInit {
 
+ 
   constructor() { }
 
   ngOnInit() {
