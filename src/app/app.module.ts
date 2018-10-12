24a69ea7fba4858239268b732b6e70cfe2362ba0
Samuel Velasquez <velasquez.samuel@correo.unimet.edu.ve>
2018-10-12 12:34:46 -0400

    arreglos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DetallesComponent } from './dashboard/detalles/detalles.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'dashboard/detalles',
+    component: DetallesComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+]
 
 @NgModule({
   declarations: [
@@ -12,7 +29,11 @@ import { DetallesComponent } from './dashboard/detalles/detalles.component';
     DetallesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
