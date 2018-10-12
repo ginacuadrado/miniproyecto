f41200f824f9ad4cc867eba811e72967d9aed40b
Samuel Velasquez <velasquez.samuel@correo.unimet.edu.ve>
2018-10-12 12:37:33 -0400

    errores

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,10 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-<<<<<<< HEAD
-    expect(page.getParagraphText()).toEqual('Welcome to pokemon!');
-=======
     expect(page.getParagraphText()).toEqual('Welcome to app!');
->>>>>>> ef17b40744a08fd3cabb9b2aa151ec2bae3132a4
   });
 });
